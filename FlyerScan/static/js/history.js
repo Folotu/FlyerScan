2954974c5166b4ba525a91036911ab249e04ebf9
austin-myhre <austinmyhre01@gmail.com>
2023-04-23 19:33:13 -0700

    display_file, History.js changes
    Added JS function to convert from 12 hr time to 24 hr time to auto populate the time fields in the display page.

--- FlyerScan/static/js/history.js
+++ FlyerScan/static/js/history.js
@@ -46,3 +46,42 @@ for (var i =0; i < editLinks.length; i++ ){
     window.location.href = flyerURL;
   });
 }
+
+// display_file.html functions
+// time conversion function from 12 hr time to 24 hr time for display
+function timeConversion(time){
+  //console.log(time);
+  var timeArray = time.split(/[: ]/);
+  var hours = timeArray[0];
+  var minutes = timeArray[1];
+  var meridian = timeArray[2];
+  if (meridian === "PM" && hours != 12){
+    hours = parseInt(hours) + 12;
+  } else if (meridian === "AM" && hours == 12){
+    hours = 0;
+  } 
+  
+  if (hours){
+    hours = hours.toString().padStart(2,'0');
+  } else{
+    hours ='00';
+  }
+
+  if(minutes){
+    minutes = minutes.toString().padStart(2,'0');
+  } else{
+    minutes='00';
+  }
+
+  convertedTime = hours + ":" + minutes;
+  //console.log(convertedTime);
+  return convertedTime;
+}
+// onload function, grabs start_time/end_time from html page
+function displayConvertedTime(){
+htmlStartTime = document.getElementById("start_time");
+htmlEndTime = document.getElementById("end_time");
+
+htmlStartTime.value = timeConversion(start_time);
+htmlEndTime.value = timeConversion(end_time);
+}
